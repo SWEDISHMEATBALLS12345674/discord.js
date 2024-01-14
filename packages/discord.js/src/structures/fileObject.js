fb3af10f8fdb2961f1a4ab6aba31ad29ab0a1882
SWEDISHMEATBALLS12345674 <72586827+SWEDISHMEATBALLS12345674@users.noreply.github.com>
2024-01-14 16:15:57 -0500

    Update fileObject.js

--- packages/discord.js/src/structures/fileObject.js
+++ packages/discord.js/src/structures/fileObject.js
@@ -1,4 +1,12 @@
+/**
+* Simple structure for file uploads
+*/
 class fileObject {
+  /**
+  *@param {string} name The name the file should show up as
+  *@param {string} contents What the file contents should be (might add a way to straight up give this a buffer)
+  *@param {string} type The filetype (as a mime type) defaults to text/plain and discord prob checks extensions anyways
+  */
   constructor(name, contents, type="text/plain") {
     this.name = name;
     this.contents = contents;
